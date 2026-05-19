67126099e78df7c8f5b002fed440d6ce6e6148f1
Arley Kahuê Valeriano da Silva <arley.kvs@gmail.com>
2026-05-19 15:37:49 -0300

    Docs: Cria o componente TodoList

---
+++ lista-de-tarefas/src/components/TodoList.jsx
@@ -0,0 +1,23 @@
+import TodoItem from "./TodoItem";
+import "../styles/TodoList.css";
+
+function TodoList({ tasks, deleteTask, toggleTask }) {
+  return (
+    <div className="todo-list">
+      {tasks.length === 0 ? (
+        <p>Nenhuma tarefa adicionada.</p>
+      ) : (
+        tasks.map((task) => (
+          <TodoItem
+            key={task.id}
+            task={task}
+            deleteTask={deleteTask}
+            toggleTask={toggleTask}
+          />
+        ))
+      )}
+    </div>
+  );
+}
+
+export default TodoList;
