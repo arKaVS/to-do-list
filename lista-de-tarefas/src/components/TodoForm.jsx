b8e5b95a4a59c94ee4410409706ab6d6ee421ec1
Arley Kahuê Valeriano da Silva <arley.kvs@gmail.com>
2026-05-19 15:36:56 -0300

    Docs: Cria o componente TodoForm.

---
+++ lista-de-tarefas/src/components/TodoForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "../styles/TodoForm.css";
+
+function TodoForm({ addTask }) {
+  const [input, setInput] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    addTask(input);
+    setInput("");
+  }
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Digite uma tarefa..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button type="submit">Adicionar</button>
+    </form>
+  );
+}
+
+export default TodoForm;
