8d48aba361c99d0b54468bb4971e0ac5eaecb89f
Arley Kahuê Valeriano da Silva <arley.kvs@gmail.com>
2026-06-05 18:44:17 -0300

    Fix: Para de mostrar as keys no console

--- lista-de-tarefas/src/lib/supabase.js
+++ lista-de-tarefas/src/lib/supabase.js
@@ -4,9 +4,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey =
   import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log(import.meta.env.VITE_SUPABASE_URL);
-console.log(import.meta.env.VITE_SUPABASE_ANON_KEY);
-
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
